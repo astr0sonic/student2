7f0725b106e208617e7392521d89035f68d39a57
KhZAfendin <KhZAfendin@IVM-14-808-06>
2024-04-05 13:18:10 +0300

    task4correct

--- volume.cpp
+++ volume.cpp
@@ -6,8 +6,8 @@ using namespace std;
 int getMaxVolume(int a, int b, int c, int n, int* t) {
 	int i = 0, j = n - 1, m = 0;
 	for (j = 0; j + a < n; j++) {
-		int k = min(t[i], t[j]);
-		if (k > m && min(t[i], t[j]) <= c) {
+		int k = min(t[j], t[j + a]);
+		if (k > m && k <= c) {
 			i = j;
 			m = k;
 		}
