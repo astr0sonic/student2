db10b66351f9739d636bf3b3be2d2513201516c5
KhZAfendin <KhZAfendin@IVM-14-808-06>
2024-04-05 13:12:29 +0300

    task4try1

---
+++ volume.cpp
@@ -0,0 +1,28 @@
+#include "volume.h"
+#include <algorithm>
+
+using namespace std;
+
+int getMaxVolume(int a, int b, int c, int n, int* t) {
+	int i = 0, j = n - 1, m = 0;
+	for (j = 0; j + a < n; j++) {
+		int k = min(t[i], t[j]);
+		if (k > m && min(t[i], t[j]) <= c) {
+			i = j;
+			m = k;
+		}
+	}
+	j = i + a;
+	if (j >= n)
+		j = n - 1;
+	while (i < j) {
+		int k = (j - i) * min(t[i], t[j]);
+		if (k > m && min(t[i], t[j]) <= c)
+			m = k;
+		if (t[i] == min(t[i], t[j]))
+			i++;
+		else
+			j--;
+	}
+	return b * m;
+}
